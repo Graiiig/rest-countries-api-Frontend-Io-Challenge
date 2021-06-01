64c0693f4d890fd2106ceb35345267fcaa3432c6
Graiiig <gregory.verguin@hotmail.fr>
2021-06-01 17:02:23 +0200

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,9 @@ const vueCountries = new Vue({
     el: '#vueContainer',
     data: {
         countries: countries,
-        theme: "dark"
+        country:country,
+        theme: "dark",
+        isShow :true
     },
     methods: {
         //function to show the countries by name
@@ -25,19 +27,36 @@ const vueCountries = new Vue({
             } else {
                     this.theme = "dark"
             }
-        }
+        },
+        //function to get details
+        getDetailsOfCountry: function (name) {
+            console.log(name)
+            fetch("https://restcountries.eu/rest/v2/name/"+name)
+            .then(response => response.json())
+            .then(
+                function (country) {
+                    vueCountries.countries = ""
+                    vueCountries.country = country
+                    vueCountries.isShow = false
+                })
+        },
+    },
+    mounted(){
+        // Load all countries when the page load the first time
+        this.all()
     }
 })
 
 // Function to show the countries depends on the filter choosen
 function showCountries(url, eventValue) {
+    
     fetch(url + eventValue)
         .then(response => response.json())
         .then(
             function (countries) {
                 vueCountries.countries = countries
+                vueCountries.country = ""
+                vueCountries.isShow = true
             })
 }
 
-// Load all countries when the page load the first time
-vueCountries.all()
